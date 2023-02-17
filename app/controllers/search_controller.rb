10e40bb201e50077128ef25fca282ef9365adc7f
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-17 10:32:09 -0700

    Fix @nation variable in index controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @nation = params[:nation]
+    @nation = params[:nation].gsub("+", " ").titleize
     @member_count = CharacterFacade.total_members_in_affiliation(@nation)
      # binding.pry
   end
