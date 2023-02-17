489e8e05318e0e4b40c59f380fe7a52a05cd0d04
Samuel Cox <samc1253@gmail.com>
2023-02-17 09:22:16 -0700

    Add first Search Test

---
+++ spec/features/search/index _spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Search Index' do
+  it 'shows all characters for a given nation' do
+    visit root_path
+    select("Fire Nation", from: "nation")
+    click_button "Search For Members"
+
+    expect(current_path).to eq(search_path)
+
+    within("#character_5cf5679a915ecad153ab68cc") do
+      expect(page).to have_content('Allies: Fire Nation')
+      expect(page).to have_content('Afiko')
+      expect(page).to have_content('Enemies: Aang')
+      expect(page).to have_content('Affiliations: Fire Nation')
+      expect(page.find('img')[:src]).to eq("https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128")
+    end
+
+    within("#character_5cf5679a915ecad153ab691d") do
+      expect(page).to have_content('Allies: None')
+      expect(page).to have_content('Ember Island Players')
+      expect(page).to have_content('Enemies: None')
+      expect(page).to have_content('Affiliations: Fire Nation')
+      expect(page.find('img')[:src]).to eq("https://vignette.wikia.nocookie.net/avatar/images/1/16/Ember_Island_Players.png/revision/latest?cb=20130722113209")
+    end
+  end
+end
