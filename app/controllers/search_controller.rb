f1f39ac4e3ecf1d1568973179f112294f67963c3
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-17 09:21:43 -0800

    Complete US

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     @members = AvatarFacade.get_nation_members(25)
-    @members_count = AvatarFacade.get_nation_members(200)
+    @members_count = AvatarFacade.get_nation_members(200).count
   end
 end
