b27d8cfc11ac46db332a865c56da5ee9726b1744
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-17 11:03:13 -0700

    fix: remove unnecessary api call for limiting array size

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     @nation_members_total = AvatarMemberFacade.all(params[:nation])
-    @nation_members = AvatarMemberFacade.all(params[:nation])[0..24]
+    @nation_members = @nation_members_total[0..24]
   end
 end
