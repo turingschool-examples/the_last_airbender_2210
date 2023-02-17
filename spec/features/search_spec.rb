7dad5a9c109e4af1c9bb74e137f660c1c929f571
Patricia Severance <patriciakseverance@gmail.com>
2023-02-17 12:02:22 -0700

    Finish tests for search page

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -9,10 +9,29 @@ RSpec.describe "when I visit '/'" do
             select('Fire Nation', from: :nation)
             click_button('Search For Members')
             expect(current_path).to eq('/search')
+            expect(page).to have_content('Characters in Nation: 97')
         end
         
         it 'has a list with detailed information for the first 25 members of the Fire Nation' do
-            
+            visit '/'
+
+            expect(page).to have_field(:nation)
+            select('Fire Nation', from: :nation)
+            click_button('Search For Members')
+            expect(current_path).to eq('/search')
+            within('#Afiko') do
+                expect(page).to have_content('Afiko')
+                expect(page).to have_content('Allies: ["Fire Nation"]')
+                expect(page).to have_content('Enemies: ["Aang"]')
+                expect(page).to have_content('Affiliation: Fire Nation')
+            end
+
+            within('#Azula') do
+                expect(page).to have_content('Azula')
+                expect(page).to have_content('Allies: ["Ozai", "Zuko "]')
+                expect(page).to have_content('Enemies: ["Iroh", "Zuko", "Kuei", "Long Feng", "Mai", "Ty Lee", "Ursa "]')
+                expect(page).to have_content("Affiliation: Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+            end
         end
     end
 end
