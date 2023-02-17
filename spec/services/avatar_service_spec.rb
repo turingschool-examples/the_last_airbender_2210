3b9822cfdcc2b69b3ae46f6fbddf287c7fdae43b
Thomas Casady <tscasady@gmail.com>
2023-02-17 11:43:34 -0600

    Remove unneeded api tests.

--- spec/services/avatar_service_spec.rb
+++ spec/services/avatar_service_spec.rb
@@ -14,31 +14,5 @@ RSpec.describe AvatarService do
       expect(response[0][:name]).to be_a String
       expect(response[0][:affiliation]).to be_a String
     end
-
-    it 'returns a parsed json response from the enemies endpoint' do
-      response = AvatarService.new.call('/api/v1/characters?enemies=Aang')
-
-      expect(response).to be_a Array
-      expect(response[0][:allies]).to be_a Array
-      expect(response[0][:allies][0]).to be_a String
-      expect(response[0][:enemies]).to be_a Array
-      expect(response[0][:enemies][0]).to be_a String
-      expect(response[0][:photoUrl]).to be_a String
-      expect(response[0][:name]).to be_a String
-      expect(response[0][:affiliation]).to be_a String
-    end
-
-    it 'returns a parsed json response from the allies endpoint' do
-      response = AvatarService.new.call('/api/v1/characters?allies=')
-
-      expect(response).to be_a Array
-      expect(response[0][:allies]).to be_a Array
-      expect(response[0][:allies][0]).to be_a String
-      expect(response[0][:enemies]).to be_a Array
-      expect(response[0][:enemies][0]).to be_a String
-      expect(response[0][:photoUrl]).to be_a String
-      expect(response[0][:name]).to be_a String
-      expect(response[0][:affiliation]).to be_a String
-    end
   end
 end
