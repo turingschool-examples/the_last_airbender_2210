8405135ab1ca9f8080609109f3ae1d3a7a95c3b1
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-17 08:48:03 -0800

    get api call working in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index 
-    
+    @nation = params[:nation]
+    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev/')
+    response = conn.get("/api/v1/characters?affiliation=#{@nation}&perPage=100")
+    @all_members = JSON.parse(response.body, symbolize_names: true)
+    @first_25_members = @all_members.first(25)
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Search results page' do 
   describe 'when I search for a member by their affiliation' do 
     it 'shows the total number of people living in that nation' do 
-      # visit root_path 
+      visit root_path 
 
-      # select 'Fire Nation', :from => 'nation'
-      # click_button "Search for Members"
+      select 'Fire Nation', :from => 'nation'
+      click_on "commit"
 
-      # expect(current_path).to eq '/search'
+      expect(current_path).to eq '/search'
 
       visit '/search'
+
+      expect(page).to have_content("Found 97 fire+nation members.")
     end
 
     xit 'should have detailed information for the first 25 members' do 
