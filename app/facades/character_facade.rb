2348d45b826169d3e5611a3d0ad326b32610ec15
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-17 10:44:10 -0700

    Adds #self.top_25_members_by_affiliation

--- app/facades/character_facade.rb
+++ app/facades/character_facade.rb
@@ -10,4 +10,11 @@ class CharacterFacade
     response = ATAService.get_members_by_affiliation(nation)
     response.count
   end
+
+  def self.top_25_members_by_affiliation(nation)
+    response = ATAService.get_members_by_affiliation(nation)
+    response[0..24].map do |character_data|
+      Character.new(character_data)
+    end
+  end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Search index" do
   describe "when visiting search after selecting a nation from '/'" do
-    it 'user sees total number of people in the fire nation' do
+    before :each do
       visit '/'
 
       select "Fire Nation", from: :nation
       click_button("Search For Members")
-
+    end
+    it 'user sees total number of people in the fire nation' do
       expect(page).to have_content("Fire Nation")
       expect(page).to have_content("Number of members: ")
     end
 
-    xit 'user sees a list of the top 25 members of the nation' do
-
+    it 'user sees a list of the top 25 members of the nation' do
+      within "#members" do
+        expect(page).to have_content("1. Afiko")
+        expect(page).to have_content("25. Fire Nave officer")
+      end
     end
 
     it  'for each member there is their name, photo, list of allies, list of enemies, and any affiliations'
