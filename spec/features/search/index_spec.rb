535af895398ab4592e8ae916888dcfb3216c32c0
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-17 09:26:55 -0700

    Adds search controller and view for its index

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Search index" do
+  describe "when visiting search after selecting a nation from '/'" do
+    it 'user sees total number of people in the fire nation' do
+
+    end
+
+    xit 'user sees a list of the top 25 members of the nation' do
+
+    end
+
+    it  'for each member there is their name, photo, list of allies, list of enemies, and any affiliations'
+  end
+end
