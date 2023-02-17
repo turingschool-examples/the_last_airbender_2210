64abaa2e473c0df3a61913af4303095c0df8fae9
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-02-17 10:51:02 -0700

    Refactor NationService

--- app/services/nation_service.rb
+++ app/services/nation_service.rb
@@ -1,18 +1,12 @@
 class NationService
-  def self.conn
-    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
-  end
-
-  def self.parse(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
   def self.get_all_nation_members_response(search_term)
+    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
+    
     response = conn.get('/api/v1/characters') do |req|
       req.params[:perPage] = 1000
       req.params[:affiliation] = search_term
     end
-    
-    parse(response)
+  
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
