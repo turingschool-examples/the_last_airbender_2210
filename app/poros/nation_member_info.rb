f4f7a4265d6068993cf642b65bc5bd0f50f0f8b4
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-02-17 13:07:13 -0500

    feat: view mostly set up; picture needed

--- app/poros/nation_member_info.rb
+++ app/poros/nation_member_info.rb
@@ -1,22 +1,23 @@
 class NationMemberInfo
-  attr_reader :id, :name, :photo, :affiliation, :enemies, :allies
+  attr_reader :id, :name, :photo, :affiliation, :allies, :enemies
   def initialize(data)
     @id = data[:_id]
     @name = data[:name]
     @photo = data[:photoUrl]
     @allies = data[:allies]
+    # require 'pry';binding.pry
     @enemies = data[:enemies]
     @affiliation = data[:affiliation]
     # require 'pry';binding.pry
   end
 
-  def allies
-    @allies.map { |allie| allie.name }
-  end
+  # def allies
+  #   @allies if @allies.count != nil
+  # end
 
-  def enemies
-    @enemies.map { |enemy| allie.name }
-  end
+  # def enemies
+  #   @enemies if @enemies.count != nil  
+  # end
 
 
 end
