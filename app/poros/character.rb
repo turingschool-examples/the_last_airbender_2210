e0943ada04510ba85a742a6cfee8d7b503163eb6
Patricia Severance <patriciakseverance@gmail.com>
2023-02-17 10:58:41 -0700

    Add affiliation attribute to character poro

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -1,5 +1,5 @@
 class Character
-    attr_reader :allies, :enemies, :name, :id, :photo_url
+    attr_reader :allies, :enemies, :name, :id, :photo_url, :affiliation
 
     def initialize(character_data)
       @id = character_data[:id]
@@ -7,5 +7,6 @@ class Character
       @enemies = character_data[:enemies]
       @name = character_data[:name]
       @photo_url = character_data[:photoUrl]
+      @affiliation = character_data[:affiliation]
     end
 end
