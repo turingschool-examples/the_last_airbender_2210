a23a634cfd238986d2fdd7d0ff4bee8580cebd62
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-17 09:18:22 -0800

    Add photos to index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,23 @@
 class SearchController < ApplicationController
   def index
     @nation = params[:nation]
-    # Faraday
 
+    # Nation member info
     conn = Faraday.new(url: "https://last-airbender-api.fly.dev") 
 
     response = conn.get("/api/v1/characters?perPage=999&page=1&affiliation=#{@nation}")
 
     json = JSON.parse(response.body, symbolize_names: true)
+
     @member_count = json.count
-    # require 'pry'; binding.pry
     @members = json.first(25)
-    # @members = []
+    
+    # # member photos
+    # @member_photos = @members.map.with_index do |member, index|
+    #   response = conn.get("/api/v1/characters/#{member[:_id]}")
+    #   json = JSON.parse(response.body, symbolize_names: true)
+    #   require 'pry'; binding.pry
+    #   json[:photoUrl]
+    # end
   end
 end
