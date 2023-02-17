4c664cbc03f3198b0fff35feba11a8f001eba878
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-17 09:01:39 -0800

    add views for each member

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -9,14 +9,24 @@ RSpec.describe 'Search results page' do
       click_on "commit"
 
       expect(current_path).to eq '/search'
-
-      visit '/search'
-
       expect(page).to have_content("Found 97 fire+nation members.")
     end
 
-    xit 'should have detailed information for the first 25 members' do 
+    it 'should have detailed information for the first 25 members' do 
+      visit root_path 
 
+      select 'Fire Nation', :from => 'nation'
+      click_on "commit"
+
+      expect(current_path).to eq '/search'
+
+      # expect(page.all('Name').count).to eq 25
+
+      within first "#member" do 
+        expect(page).to have_content("Name: Afiko")
+        expect(page).to have_content("Allies: Fire Nation")
+        expect(page).to have_content("Enemies: Aang")
+      end
     end
   end
 end
