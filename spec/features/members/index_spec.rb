5ad256f887ab9e6da24381702d83b7b8ee5d8a78
Matisse Mallette <matissemallette@gmail.com>
2023-02-17 11:20:08 -0700

    Add enemies and allies

--- spec/features/members/index_spec.rb
+++ spec/features/members/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'members index', type: :feature do
     it 'displays list of members' do 
 
       visit '/search?nation=Fire+Nation'
-      save_and_open_page
+      
       expect(page).to have_content 'Fire Nation'
       expect(page).to have_content 'Number of members: 97'
 
