9b7256d1c312fd1abc84bf83d60c4552b9594a8a
Ryan Canton <ryancanton999@gmail.com>
2023-02-17 10:31:54 -0700

    Test: Add test for user story feature

--- spec/features/fire_nation_spec.rb
+++ spec/features/fire_nation_spec.rb
@@ -1,14 +1,25 @@
-# As a user,
-# When I visit "/"
-# And I Select "Fire Nation" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-# And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+require 'rails_helper'
 
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies or "None"
-# - The list of enemies or "None"
-# - Any affiliations that the member has
+RSpec.describe "Fire Nation Search" do
+  it 'displays the total count of fire nation members, and details on the first 25' do
+    json_response = File.read('spec/fixtures/fire_nation_members.json')
+
+    stub_request(:get, "https://last-airbender-api.fly.dev/api/v1/characters?perPage=900&page=1&affiliation=fire+nation")
+      .to_return(status: 200, body: json_response, headers: {})
+
+    visit "/"
+    select "Fire Nation"
+    click_button "Search"
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("Total Members: 97")
+    expect(page).to have_content("Name: ", count: 25)
+    within "div#5cf5679a915ecad153ab68da" do
+      expect(page).to have_content("Name: Azula")
+      expect(page).to have_content("Enemies: Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa")
+      expect(page).to have_content("Allies: Ozai, Zuko")
+      expect(page).to have_content("Affiliation: Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+      expect(page).to have_css("img[src*='https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941']")
+    end
+  end
+end
