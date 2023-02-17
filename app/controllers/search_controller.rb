5e1e6081bf40ce92d6de474e5c30fb9b85a7d465
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-02-17 09:59:40 -0800

    Refactor member search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @nation = params[:nation]
-
-    conn = Faraday.new(url: "https://last-airbender-api.fly.dev") 
-
-    response = conn.get("/api/v1/characters?perPage=999&page=1&affiliation=#{@nation}")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @member_count = json.count
-    @members = json.first(25)
-
-    @members.each do |member|
-      if member[:allies].count == 0
-        member[:allies] = "None"
-      end
-      if member[:enemies].count == 0
-        member[:enemies] = "None"
-      end
+    @members = MemberSearchFacade.member_search(params[:nation])
     end
   end
 end
--- app/facades/member_search_facade.rb
+++ app/facades/member_search_facade.rb
@@ -1,5 +1,23 @@
 class MemberSearchFacade
   def self.member_search(nation) 
-    []
+    conn = Faraday.new(url: "https://last-airbender-api.fly.dev") 
+
+    response = conn.get("/api/v1/characters?perPage=999&page=1&affiliation=#{@nation}")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @member_count = json.count
+    @members = json.first(25).map do |member|
+      Member.new(member)
+    end
+
+    @members.each do |member|
+      if member[:allies].count == 0
+        member[:allies] = "None"
+      end
+      if member[:enemies].count == 0
+        member[:enemies] = "None"
+      end
+    end
   end
 end
