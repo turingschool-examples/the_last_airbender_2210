161cb04417174d22233b9921850a275738817d63
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-20 18:41:29 -0700

    refactor: remove comments from controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,7 @@
 class SearchController < ApplicationController
   def index
     @searched_nation = params[:nation]
-    # conn = Faraday.new("https://last-airbender-api.fly.dev/api/v1/")
-    # response = conn.get("characters?affiliation=#{@searched_nation}")
-    # parse = JSON.parse(response.body, symbolize_names: true)
     @nations = NationFacade.nation_search(@searched_nation)
-    # @total_members = NationFacade.parse_json.count
-    # @member_list = parse.first(25)
     @nation = @searched_nation.split("+").map(&:capitalize).join(' ')
   end
 end
